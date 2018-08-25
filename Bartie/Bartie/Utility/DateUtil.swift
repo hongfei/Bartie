6404662173ac8e468cd418365e7b801d4830226f
Hongfei Zhou <mich.zhou1989@gmail.com>
2018-08-24 19:34:01 -0700

    update

--- Bartie/Bartie/Utility/DateUtil.swift
+++ Bartie/Bartie/Utility/DateUtil.swift
@@ -9,6 +9,7 @@ class DateUtil {
     static let dateFormatter = DateFormatter()
     static func getTimeDifferenceToNow(dateString: String) -> Int {
         self.dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        self.dateFormatter.timeZone = TimeZone(identifier: "America/Los_Angeles")
         self.dateFormatter.locale = Locale(identifier: "en_US_POSIX")
 
         if let tripTime = self.dateFormatter.date(from: dateString) {
