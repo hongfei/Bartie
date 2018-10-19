1af7d5682401b699bc5797cd1a7e1f6b163a1dc5
Hongfei Zhou <hongfzhou@ebay.com>
2018-10-19 16:35:58 -0700

    [Refactor] Remove the favorite list and replace with table view controller

--- Bartie/Bartie/Controller/Favorite/FavoriteViewController.swift
+++ Bartie/Bartie/Controller/Favorite/FavoriteViewController.swift
@@ -8,80 +8,139 @@ import SwiftIcons
 import UIColor_Hex_Swift
 import PinLayout
 
-class FavoriteViewController: UIViewController, FavoriteListViewDelegate {
-    var favoriteList: FavoriteListView!
+class FavoriteViewController: UITableViewController, FavoriteListHeaderDelegate {
+    var favorites: [Favorite] = []
+    var tripsListMap: [Int: [(Trip, Departure?)]] = [:]
+    var inDeleteFavoriteMode: Bool = false
 
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 
     init() {
         super.init(nibName: nil, bundle: nil)
         self.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
         self.view.backgroundColor = .white
-    }
-
-    override var navigationItem: UINavigationItem {
-        let navItem = UINavigationItem(title: "Favorites")
-        navItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self.favoriteList, action: #selector(FavoriteListView.editFavorites))
-        navItem.rightBarButtonItem?.tintColor = .white
-        return navItem
+        self.title = "Favorites"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editFavorites))
+        self.navigationItem.rightBarButtonItem?.tintColor = .white
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.favoriteList = FavoriteListView()
-        self.favoriteList.favoriteListDelegate = self
-        self.favoriteList.favorites = FavoriteService.getAllFavorites()
+        self.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        self.view.backgroundColor = .white
 
-        self.view.addSubview(self.favoriteList)
-        self.onRefreshList()
+        self.tableView.register(TripListCell.self, forCellReuseIdentifier: "TripListCell")
+        self.tableView.register(FavoriteListHeader.self, forHeaderFooterViewReuseIdentifier: "FavoriteListHeader")
+        self.tableView.tableFooterView = UIView(frame: .zero)
+
+        self.favorites = FavoriteService.getAllFavorites()
+
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.addTarget(self, action: #selector(refreshTable), for: .valueChanged)
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        self.favoriteList.pin.all()
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.favorites.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let tripList = self.tripsListMap[section] {
+            return min(Settings.favoriteTripCount, tripList.count)
+        }
+        return Settings.favoriteTripCount
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TripListCell", for: indexPath)
+        guard let favoriteCell = cell as? TripListCell, let tripList = self.tripsListMap[indexPath.section] else {
+            return cell
+        }
+
+        favoriteCell.trip = tripList[indexPath.row].0
+        favoriteCell.departure = tripList[indexPath.row].1
+        favoriteCell.station = self.favorites[indexPath.section].station
+        favoriteCell.destination = self.favorites[indexPath.section].destination
+        favoriteCell.reloadTripData()
+        return favoriteCell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let trips = self.tripsListMap[indexPath.section] else {
+            return 0
+        }
+
+        return CGFloat(trips[indexPath.row].0.leg.count * 20 + 70)
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return FavoriteListHeader.HEIGHT
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let cellHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "FavoriteListHeader") as? FavoriteListHeader else {
+            return nil
+        }
+
+        let favorite = self.favorites[section]
+        cellHeader.favoriteListHeaderDelegate = self
+        cellHeader.setData(favorite: favorite, isDeleting: self.inDeleteFavoriteMode)
+        return cellHeader
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        if FavoriteService.getAllFavorites().count > self.favoriteList.favorites.count {
-            self.onRefreshList()
+        if FavoriteService.getAllFavorites().count > self.favorites.count {
+            self.refreshTable()
         }
     }
 
-    func onRefreshList() {
-        let favorites = FavoriteService.getAllFavorites()
-        self.favoriteList.favorites = favorites
-        var refreshCount = favorites.count
+    @IBAction func refreshTable() {
+        self.favorites = FavoriteService.getAllFavorites()
 
-        if refreshCount == 0 {
-            self.favoriteList.refreshControl?.endRefreshing()
-            return
+        if self.favorites.count == 0 {
+            self.refreshControl?.endRefreshing()
         }
 
-        for (index, favorite) in favorites.enumerated() {
+        var refreshCount = self.favorites.count
+        for (index, favorite) in self.favorites.enumerated() {
             ScheduleService.getTripPlan(from: favorite.station, to: favorite.destination, beforeCount: 1, afterCount: 2) { trips in
                 RealTimeService.getSelectedDepartures(for: favorite.station) { departures in
-                    self.favoriteList.tripsListMap[index] = DataUtil.regulateTripsWithDepartures(for: trips, with: departures)
+                    self.tripsListMap[index] = DataUtil.regulateTripsWithDepartures(for: trips, with: departures)
                     refreshCount -= 1
                     if refreshCount == 0 {
-                        self.favoriteList.reloadData()
-                        self.favoriteList.refreshControl?.endRefreshing()
+                        self.tableView.reloadData()
+                        self.refreshControl?.endRefreshing()
                     }
                 }
             }
         }
     }
 
-    func onSelectRow(for trip: Trip, with departure: Departure?, from favorite: Favorite) {
-        let favoriteDetail = FavoriteDetailViewController()
-        favoriteDetail.trip = trip
-        favoriteDetail.departure = departure
-        favoriteDetail.favorite = favorite
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath), let favoriteCell = cell as? TripListCell {
+            let favoriteDetail = FavoriteDetailViewController()
+            favoriteDetail.trip = favoriteCell.trip
+            favoriteDetail.departure = favoriteCell.departure
+            favoriteDetail.favorite = self.favorites[indexPath.section]
+            self.present(FavoriteDetailNavigationViewController(rootViewController: favoriteDetail), animated: true)
 
-        self.present(FavoriteDetailNavigationViewController(rootViewController: favoriteDetail), animated: true)
+            favoriteCell.setSelected(false, animated: true)
+        }
+    }
+
+    @IBAction func editFavorites(_ sender: UIBarButtonItem) {
+        self.inDeleteFavoriteMode = !self.inDeleteFavoriteMode
+        sender.title = self.inDeleteFavoriteMode ? "Done" : "Edit"
+        self.tableView.reloadData()
+    }
+
+    func onDeleteFavorite(favorite: Favorite) {
+        FavoriteService.deleteFavorite(favorite: favorite)
+        self.favorites = FavoriteService.getAllFavorites()
+        self.tableView.reloadData()
     }
 }
